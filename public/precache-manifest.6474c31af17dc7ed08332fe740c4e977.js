20d72671cd1e05aaf48d06db2b71312d015b1bef
pheno <nicolas.enault@gmail.com>
2020-11-28 11:14:02 +0100

    first deploy

---
+++ public/precache-manifest.6474c31af17dc7ed08332fe740c4e977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f428dc3992737a635b4d4dd0c1a8bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c74a51f8940bb86b423",
+    "url": "/static/css/main.6537bc47.chunk.css"
+  },
+  {
+    "revision": "dc257caf175baf21b802",
+    "url": "/static/js/2.51587361.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.51587361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c74a51f8940bb86b423",
+    "url": "/static/js/main.2747eb2e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
